c7ada5c60f27e17355b2e6f69a5b089134a22ba8
moulali <moulalimoula190@gmail.com>
2025-04-21 13:32:57 +0530

    completing not found page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import NotFound from "./components/pages/customer/NotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/not-found" element={<NotFound />} />
+        {/* Redirect all unknown routes to /not-found */}
+        <Route path="*" element={<Navigate to="/not-found" replace />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/components/pages/customer/NotFound/index.js
@@ -0,0 +1,19 @@
+import "./index.css";
+
+const NotFound = () => (
+  <div className="not_found_container">
+    <img
+      src="https://res.cloudinary.com/dssaftaaa/image/upload/v1745218216/page_not_found_mkifkj.png"
+      alt="not found"
+      className="not-found-img"
+    />
+    <h1>Page Not Found</h1>
+    <p>
+      we are sorry, the page you requested could not be found, Please go back to
+      the homepage.
+    </p>
+    <button>Go Back to Home</button>
+  </div>
+);
+
+export default NotFound;
